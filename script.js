0fca3f8983974caee1b4827e932b56498db5ed57
MemoryFr <38227495+MemoryFr@users.noreply.github.com>
2020-12-01 14:52:49 +0300

    upd

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-            let previousColor = localStorage.getItem(0);
-            let SSD = document.getElementsByClassName("logo")[0];
-            console.log(SSD);
-            var getPreviousBackground = function () {
+    // Back-to-top button //
+    $('<a id="button"></a>').prependTo('body');
 
-                document.body.style.background = previousColor+" url(images/space.svg)";
-            }
-            
-            getPreviousBackground();
-            
-            var changeBackground = function() {
-                let randomColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-                localStorage.setItem(0, randomColor);
-                document.body.style.background = randomColor+" url(images/space.svg)";
-            }
-            
-            var resetBackground = function() {
-                localStorage.setItem(0, "#fff");
-                document.body.style.background = "#fff url(images/space.svg)";
-                return false;
-            }
-           
-                SSD.addEventListener('contextmenu', function(ev) {
-                ev.preventDefault();
-                resetBackground();
-                return false;
-            }, false);
-            
-        
+    var btn = $('#button');
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 500) {
+            btn.addClass('show');
+        } 
+        else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, 1000);
+    });    
+
+    // Background //
+
+    let previousColor = localStorage.getItem(0);
+    let SSD = document.getElementsByClassName("logo")[0];
+    var getPreviousBackground = function () {
+
+        document.body.style.background = previousColor+" url(images/space.svg)";
+    }
+
+    getPreviousBackground();
+
+    var changeBackground = function() {
+        let randomColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+        localStorage.setItem(0, randomColor);
+        document.body.style.background = randomColor+" url(images/space.svg)";
+    }
+
+    var resetBackground = function() {
+        localStorage.setItem(0, "#fff");
+        document.body.style.background = "#fff url(images/space.svg)";
+        return false;
+    }
+
+        SSD.addEventListener('contextmenu', function(ev) {
+        ev.preventDefault();
+        resetBackground();
+        return false;
+    }, false);
